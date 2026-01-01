7c8e394105cecbd24d094e2285459de858a5688a
pderyuga <polina.deryuga@persuit.com>
2026-01-01 14:19:29 -0500

    feat: convert text to lowercase before matching

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -6,10 +6,17 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT):
 
     matches = []
     for movie in movies:
-        if query in movie["title"]:
+        preprocessed_query = preprocess_text(query)
+        preprocessed_title = preprocess_text(movie["title"])
+        if preprocessed_query in preprocessed_title:
             matches.append(movie)
 
     sorted_matches = sorted(matches, key=lambda match: match["id"])
     sliced_matches = sorted_matches[:5]
 
     return sliced_matches
+
+
+def preprocess_text(text: str):
+    text = text.lower()
+    return text
