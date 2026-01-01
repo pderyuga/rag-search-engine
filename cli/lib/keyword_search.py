b75c8bf49dc3eb665be6ea23f3490cdc4f924bea
pderyuga <polina.deryuga@persuit.com>
2026-01-01 15:03:14 -0500

    feat: tokenize text and match the tokens

--- cli/lib/keyword_search.py
+++ cli/lib/keyword_search.py
@@ -7,15 +7,24 @@ def search_command(query: str, limit: int = DEFAULT_SEARCH_LIMIT):
 
     matches = []
     for movie in movies:
-        preprocessed_query = preprocess_text(query)
-        preprocessed_title = preprocess_text(movie["title"])
-        if preprocessed_query in preprocessed_title:
+        query_tokens = tokenize_text(query)
+        title_tokens = tokenize_text(movie["title"])
+        is_item_present = has_matching_token(query_tokens, title_tokens)
+        if is_item_present:
             matches.append(movie)
 
     sorted_matches = sorted(matches, key=lambda match: match["id"])
-    sliced_matches = sorted_matches[:5]
+    sliced_matches = sorted_matches[:limit]
 
     return sliced_matches
+
+
+def has_matching_token(query_tokens: list[str], title_tokens: list[str]):
+    for query_token in query_tokens:
+        for title_token in title_tokens:
+            if query_token in title_token:
+                return True
+    return False
 
 
 def preprocess_text(text: str):
@@ -23,3 +32,13 @@ def preprocess_text(text: str):
     translation_table = str.maketrans("", "", string.punctuation)
     translated_text = text.translate(translation_table)
     return translated_text
+
+
+def tokenize_text(text: str):
+    preprocessed_text = preprocess_text(text)
+    tokens = preprocessed_text.split(" ")
+    non_empty_tokens = []
+    for token in tokens:
+        if token:
+            non_empty_tokens.append(token)
+    return non_empty_tokens
